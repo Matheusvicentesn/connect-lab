0b240d1620560db299fd9be4ea53233e3ae0ddc1
Matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-09-12 18:44:38 -0300

    perfil

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -1,9 +1,33 @@
 import { Card } from "../../components/Card/Card";
+import avatar from "../../assets/img/avatar.png";
+import { CardStyled } from "./Perfil.styles";
+import { Link } from "react-router-dom";
 
 export const Perfil = () => {
   return (
     <Card>
-      <div>teste</div>
+      <CardStyled>
+        <div className="container">
+          <h2>Perfil</h2>
+          <article>
+            <img src={avatar} alt="" />
+            <p>Nome Completo</p>
+            <p>Email@email.com - (11)0000-0000</p>
+          </article>
+          <h2>Endereço</h2>
+          <hr />
+          <p>Avenida Avenida numero 000 - Bairro Bairro - 85500-000</p>
+          <br/>
+          <div className="footer">
+            <button className="butao">Editar</button>
+            <br></br>
+            <Link to={"/"}>
+              Sair &nbsp;
+              <i className="fa-solid fa-arrow-up-right-from-square"></i>
+            </Link>
+          </div>
+        </div>
+      </CardStyled>
     </Card>
   );
 };
---
+++ src/pages/Perfil/Perfil.styles.jsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const CardStyled = styled.html`
+  h2 {
+    margin-bottom: 15px;
+    color: white;
+    text-align: center;
+  }
+  .container {
+    background: #002630;
+    max-width: 500px;
+    margin: 20vh auto;
+    padding: 45px;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    /* border: solid 1px red; */
+  }
+
+  .footer{
+    text-align: center;
+  }
+
+  article {
+    width: 100%;
+    /* border: solid 1px red; */
+    margin-bottom: 50px;
+  }
+
+  img {
+    height: 50px;
+    float: left;
+    border-radius: 5px;
+    margin-right: 50px;
+  }
+
+  p {
+    color: #92c1d4;
+    font-size: 20px;
+  }
+
+  hr {
+    border-top: 1px solid #92c1d4;
+    margin-bottom: 5px;
+  }
+
+  .butao {
+    background-color: #d37b16;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    margin-bottom: 14px;
+  }
+
+  a {
+    text-decoration: none;
+    font-size: 20px;
+    color: #f2f2f2;
+  }
+`;
