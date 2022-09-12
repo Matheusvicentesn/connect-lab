cc43815a5459bb8438a72e5eb6d27426d829b68f
matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-09-12 13:57:55 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+// import "./App.css";
+import { Header } from "./components/Header/Header";
+import { GlobalStyle } from "./components/Theme/GlobalStyle";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
