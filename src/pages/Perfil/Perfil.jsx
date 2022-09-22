f31e1450238bcff65200236a4ec078db9af4a84a
Matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-09-22 20:56:14 -0300

    alguns ajustes

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -1,11 +1,10 @@
 import { Card } from "../../components/Card/Card";
-import { CardStyled, ModalPerfil } from "./Perfil.styles";
+import { CardStyled } from "./Perfil.styles";
 import { Context } from "../../context/autenticacao/app-context";
 import { useContext, useState, useEffect } from "react";
 import Loading from "../../components/Loading/Loading";
 import { buscarPerfil, cadastrarUsuario } from "../../services/api";
 import Modal from "../../components/Modal/Modal";
-import { Form } from "../../components/Form/Form";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -65,7 +64,7 @@ export const Perfil = () => {
   }, []);
 
   const { auth, handleLogout } = useContext(Context);
-  console.log(auth);
+  console.log(storageValues, auth);
 
   // Buscar Perfil
   const [usuario, setUsuario] = useState();
@@ -79,130 +78,125 @@ export const Perfil = () => {
 
   return (
     <>
-      <ModalPerfil>
+      <>
         <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-          <Form>
-            <div>
-              <h2>Cadastro</h2>
-              <br />
-              <form onSubmit={handleSubmit(post)}>
-                <ul>
-                  <li>
-                    <label htmlFor="name">
-                      Nome Completo* <p>{errors.name?.message}</p>
-                    </label>
-                    <input type="text" name="name" {...register("name")} />
-                  </li>
-                  <li>
-                    <label htmlFor="email">
-                      E-mail* <p>{errors.email?.message}</p>
-                    </label>
-                    <input type="text" name="email" {...register("email")} />
-                  </li>
-                  <li>
-                    <label htmlFor="pic">
-                      URL foto de perfil <p>{errors.pic?.message}</p>
-                    </label>
-                    <input type="text" name="pic" {...register("pic")} />
-                  </li>
-                  <li>
-                    <label htmlFor="phone">
-                      Telefone <p>{errors.phone?.message}</p>
-                    </label>
-                    <input type="text" name="phone" {...register("phone")} />
-                  </li>
-                  <li>
-                    <label htmlFor="password">
-                      Senha* <p>{errors.password?.message}</p>
-                    </label>
-                    <input
-                      type="text"
-                      name="password"
-                      {...register("password")}
-                    />
-                  </li>
-                  <li>
-                    <label htmlFor="confirmPassowrd">
-                      Confirmação da senha*{" "}
-                      <p>{errors.confirmPassowrd?.message}</p>
-                    </label>
-                    <input
-                      type="text"
-                      name="confirmPassowrd"
-                      {...register("confirmPassowrd")}
-                    />
-                  </li>
-                  <li>
-                    <label htmlFor="zipCode">
-                      CEP* <p>{errors.zipCode?.message}</p>
-                    </label>
-                    <input
-                      type="text"
-                      name="zipCode"
-                      {...register("zipCode")}
-                    />
-                  </li>
-                  <li>
-                    <label htmlFor="adress">
-                      Logradouro/Endereço* <p>{errors.adress?.message}</p>
-                    </label>
-                    <input type="text" name="adress" {...register("adress")} />
-                  </li>
-                  <li>
-                    <label htmlFor="city">
-                      Cidade* <p>{errors.city?.message}</p>
-                    </label>
-                    <input type="text" name="city" {...register("city")} />
-                  </li>
-                  <li>
-                    <label htmlFor="district">
-                      Bairro* <p>{errors.district?.message}</p>
-                    </label>
-                    <input
-                      type="text"
-                      name="district"
-                      {...register("district")}
-                    />
-                  </li>
-                  <li>
-                    <label htmlFor="state">
-                      Estado* <p>{errors.state?.message}</p>
-                    </label>
-                    <input type="text" name="state" {...register("state")} />
-                  </li>
-                  <li>
-                    <label htmlFor="complement">
-                      Complemento <p>{errors.complement?.message}</p>
-                    </label>
-                    <input
-                      type="text"
-                      name="complement"
-                      {...register("complement")}
-                    />
-                  </li>
-                  <li>
-                    <label htmlFor="houseNumber">
-                      Número* <p>{errors.houseNumber?.message}</p>
-                    </label>
-                    <input
-                      type="number"
-                      name="houseNumber"
-                      {...register("houseNumber")}
-                    />
-                  </li>
-                  <button type="submit">Cadastrar</button>
-                  <br />
-                  <br />
-                  <Link to={"/"}>
-                    Login &nbsp;
-                    <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                  </Link>
-                </ul>
-              </form>
-            </div>
-          </Form>
+          <div>
+            <h2>Cadastro</h2>
+            <br />
+            <form onSubmit={handleSubmit(post)}>
+              <ul>
+                <li>
+                  <label htmlFor="name">
+                    Nome Completo* <p>{errors.name?.message}</p>
+                  </label>
+                  <input type="text" name="name" {...register("name")} />
+                </li>
+                <li>
+                  <label htmlFor="email">
+                    E-mail* <p>{errors.email?.message}</p>
+                  </label>
+                  <input type="text" name="email" {...register("email")} />
+                </li>
+                <li>
+                  <label htmlFor="pic">
+                    URL foto de perfil <p>{errors.pic?.message}</p>
+                  </label>
+                  <input type="text" name="pic" {...register("pic")} />
+                </li>
+                <li>
+                  <label htmlFor="phone">
+                    Telefone <p>{errors.phone?.message}</p>
+                  </label>
+                  <input type="text" name="phone" {...register("phone")} />
+                </li>
+                <li>
+                  <label htmlFor="password">
+                    Senha* <p>{errors.password?.message}</p>
+                  </label>
+                  <input
+                    type="text"
+                    name="password"
+                    {...register("password")}
+                  />
+                </li>
+                <li>
+                  <label htmlFor="confirmPassowrd">
+                    Confirmação da senha*{" "}
+                    <p>{errors.confirmPassowrd?.message}</p>
+                  </label>
+                  <input
+                    type="text"
+                    name="confirmPassowrd"
+                    {...register("confirmPassowrd")}
+                  />
+                </li>
+                <li>
+                  <label htmlFor="zipCode">
+                    CEP* <p>{errors.zipCode?.message}</p>
+                  </label>
+                  <input type="text" name="zipCode" {...register("zipCode")} />
+                </li>
+                <li>
+                  <label htmlFor="adress">
+                    Logradouro/Endereço* <p>{errors.adress?.message}</p>
+                  </label>
+                  <input type="text" name="adress" {...register("adress")} />
+                </li>
+                <li>
+                  <label htmlFor="city">
+                    Cidade* <p>{errors.city?.message}</p>
+                  </label>
+                  <input type="text" name="city" {...register("city")} />
+                </li>
+                <li>
+                  <label htmlFor="district">
+                    Bairro* <p>{errors.district?.message}</p>
+                  </label>
+                  <input
+                    type="text"
+                    name="district"
+                    {...register("district")}
+                  />
+                </li>
+                <li>
+                  <label htmlFor="state">
+                    Estado* <p>{errors.state?.message}</p>
+                  </label>
+                  <input type="text" name="state" {...register("state")} />
+                </li>
+                <li>
+                  <label htmlFor="complement">
+                    Complemento <p>{errors.complement?.message}</p>
+                  </label>
+                  <input
+                    type="text"
+                    name="complement"
+                    {...register("complement")}
+                  />
+                </li>
+                <li>
+                  <label htmlFor="houseNumber">
+                    Número* <p>{errors.houseNumber?.message}</p>
+                  </label>
+                  <input
+                    type="number"
+                    name="houseNumber"
+                    {...register("houseNumber")}
+                  />
+                </li>
+                <button type="submit">Cadastrar</button>
+                <br />
+                <br />
+                <Link to={"/"}>
+                  Login &nbsp;
+                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                </Link>
+              </ul>
+            </form>
+          </div>
         </Modal>
-      </ModalPerfil>
+      </>
+
       <Card>
         <CardStyled>
           <div className="container">
--- src/pages/Perfil/Perfil.styles.jsx
+++ src/pages/Perfil/Perfil.styles.jsx
@@ -63,17 +63,55 @@ export const CardStyled = styled.div`
   }
 `;
 
-export const ModalPerfil = styled.div`
-  Modal {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: #ebeeee;
-    padding: 50px;
-    z-index: 1000;
-    height: 800px;
-    width: 900px;
-    border-radius: 10px;
+export const Modal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #ebeeee;
+  padding: 50px;
+  z-index: 1000;
+  height: 400px;
+  width: 400px;
+  border-radius: 10px;
+
+  input {
+    display: block;
+    margin-top: 10px;
+    width: 100%;
+  }
+  select {
+    width: 100%;
+  }
+
+  img {
+    width: auto;
+    height: 80px;
+  }
+  Button {
+    background-color: #d37b16;
+    border-radius: 18px;
+    border: none;
+    color: white;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
   }
 `;
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+`;
+
+
+
