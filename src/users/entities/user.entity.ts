f001140a38a0b5f0445fa03e202f0367dde1c81e
Matheus Vicente <zero@pop-os.localdomain>
2023-01-07 18:43:46 -0300

    entidade de usuários

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50, nullable: false })
+  name: string;
+
+  @Column({ length: 50, unique: true })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: false })
+  salt: string;
+
+  @Column({ type: 'varchar', length: 64 })
+  confirmationToken: string;
+
+  @Column({ type: 'varchar', length: 64 })
+  recoverToken: string;
+
+  @Column({ length: 50, nullable: false })
+  phone: string;
+}
