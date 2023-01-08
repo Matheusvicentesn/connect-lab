3618ea56ab7b8e221cd75d04c9fd09247fa97c60
Matheus Vicente <zero@pop-os.localdomain>
2023-01-08 13:50:12 -0300

    deleting the auth.module was breaking the code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { databaseProviders } from './core/database/database.provider';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,7 +10,6 @@ import { AuthModule } from './auth/auth.module';
       envFilePath: '.env',
       isGlobal: true,
     }),
-    AuthModule,
   ],
   controllers: [],
   providers: [...databaseProviders],
