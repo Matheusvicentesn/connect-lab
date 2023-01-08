76cb7e361d94b616b109f0fa31c636164fa72847
Matheus Vicente <zero@pop-os.localdomain>
2023-01-08 14:40:37 -0300

    signUp function on user.service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,15 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { databaseProviders } from 'src/core/database/database.provider';
 import { userProviders } from './users;provider';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, ...databaseProviders, ...userProviders],
+  providers: [
+    AuthService,
+    UsersService,
+    ...databaseProviders,
+    ...userProviders,
+  ],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserEntity } from './entities/user.entity';
 
 @Injectable()
@@ -7,9 +9,14 @@ export class UsersService {
   constructor(
     @Inject('USER_REPOSITORY')
     private userRepository: Repository<UserEntity>,
+    private authService: AuthService,
   ) {}
 
   async findAll() {
     return await this.userRepository.find({ relations: { address: true } });
+  }
+
+  async signUp(user: CreateUserDto) {
+    this.authService.signUp(user);
   }
 }
