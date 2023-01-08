a03b787484818cc54ef2061f131a1f622d161cec
Matheus Vicente <zero@pop-os.localdomain>
2023-01-08 14:32:26 -0300

    signUp function create

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { databaseProviders } from './core/database/database.provider';
-import { AuthModule } from './auth/auth.module';
-
 @Module({
   imports: [
     UsersModule,
@@ -11,7 +9,6 @@ import { AuthModule } from './auth/auth.module';
       envFilePath: '.env',
       isGlobal: true,
     }),
-    AuthModule,
   ],
   controllers: [],
   providers: [...databaseProviders],
