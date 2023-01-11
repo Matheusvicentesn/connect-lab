b48929916953795842f14b5d7516185313f84a09
Matheus Vicente <zero@pop-os.localdomain>
2023-01-11 11:27:32 -0300

    cleaning resource and create entity devices

---
+++ src/core/database/migrations/1673447195672-devices_Entity.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class devicesEntity1673447195672 implements MigrationInterface {
+    name = 'devicesEntity1673447195672'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "devices" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "type" character varying NOT NULL, "madeBy" character varying NOT NULL, "photoUrl" character varying NOT NULL, CONSTRAINT "PK_b1514758245c12daf43486dd1f0" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "devices"`);
+    }
+
+}
--- src/devices/entities/device.entity.ts
+++ src/devices/entities/device.entity.ts
@@ -1,1 +1,19 @@
-export class Device {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'devices' })
+export class DeviceEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column()
+  type: string;
+
+  @Column()
+  madeBy: string;
+
+  @Column()
+  photoUrl: string;
+}
